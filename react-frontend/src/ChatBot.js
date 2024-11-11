089c28af398877fd20745d52f00cd8a297396465
Walland3r <oswalawender@gmail.com>
2024-11-11 23:28:16 +0100

    Improved icons and animations

--- react-frontend/src/ChatBot.js
+++ react-frontend/src/ChatBot.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 
 const ChatBot = () => {
     const [input, setInput] = useState('');
-
     const handleSendMessage = () => {
         document.querySelector(".info-container").classList.add("info-hidden")
+        document.querySelector(".chatbot-output").classList.add("visible")
     };
+
+    function resizeInput(el) {
+        el.style.width = el.value.length + "ch";
+    }
 
     return (
         <div className="chatbot-container">
@@ -14,9 +18,13 @@ const ChatBot = () => {
                     type="text"
                     placeholder="Input text"
                     value={input}
-                    onChange={(e) => setInput(e.target.value)}
+                    onChange={(e) => {setInput(e.target.value); resizeInput(e.target);}}
                     onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
+                    maxLength="120"
                 />
+            </div>
+            <div className="chatbot-output">
+                <div className="loader"></div>
             </div>
         </div>
     );
--- react-frontend/src/Header.js
+++ react-frontend/src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './styles.css';
-import logo from "./images/logo-cropped.svg"
+import logo from "./images/logo.svg"
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} alt="Logo"/>
+      <div className='header-left-side'>
+        <img src={logo} alt="Logo"/>
+        <p>DB Search Assistant</p>
+      </div>
       <div className='header-menu'>
-        <a href='https://github.com/wiktorlewandowski9/db-search-assistant'>Start</a>
+        <a href='http://localhost:3000'>Start</a>
         <a href='https://github.com/wiktorlewandowski9/db-search-assistant'>Github</a>
         <a href='https://github.com/wiktorlewandowski9/db-search-assistant'>User</a>
       </div>
